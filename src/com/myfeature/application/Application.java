8ab39795516de004247f7245a876ce0f41b4efe3
millet.ua.su@gmail.com <millet.ua.su@gmail.com>
2019-03-12 23:32:02 +0200

    Implemented default approach
    Implemented default approach in application

---
+++ src/com/myfeature/application/Application.java
@@ -0,0 +1,24 @@
+package com.myfeature.application;
+
+public class Application {
+	
+	public double plus(double a, double b) {
+		return a + b;		
+	}
+	
+	public double minus(double a, double b) {
+		return a - b;		
+	}
+	
+	public double multiply(double a, double b) {
+		return a * b;		
+	}
+	
+	public double divide(double a, double b) {
+		return a / b;		
+	}
+	
+	public double sqrt(double a) {
+		return Math.sqrt(a);
+	}
+}
--- src/com/myfeature/demo/Demo.java
+++ src/com/myfeature/demo/Demo.java
@@ -1,11 +1,29 @@
 package com.myfeature.demo;
+import java.util.ArrayList;
+import java.util.List;
+import com.myfeature.application.Application;
 
 public class Demo {
+	
 	public static void main(String[] args) {
 		new Demo().start();
 	}
 	
+	private final String line = "--------------------------------------";		
+	
+	private Application app = new Application();
+	
 	public void start() {
-		
+		System.out.println("Start demo:");
+		System.out.println(line);
+				
+		System.out.println(app.plus(28, 31));		
+		System.out.println(app.minus(13, 26));
+		System.out.println(app.multiply(12, 12));		
+		System.out.println(app.divide(45, 90));		
+		System.out.println(app.sqrt(81));		
+		System.out.println(line);		
+		System.out.println(app.divide(45, 0));
+		System.out.println(app.sqrt(-81));	
 	}
 }
