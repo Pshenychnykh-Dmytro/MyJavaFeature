e30fef33b0d8b95257feea4bc7ff411e091eade2
millet.ua.su@gmail.com <millet.ua.su@gmail.com>
2019-03-20 22:16:03 +0200

    Using abstract class
    Using abstract, inheritance, polymorphism

--- src/com/myfeature/application/Application.java
+++ src/com/myfeature/application/Application.java
@@ -11,9 +11,85 @@ public class Application {
 	//delay simulation
 	private int delay = 2000;
 	
-	public void plus(double a, double b) {
+	abstract class Function{
+		protected double a;
+		protected double b;
+		
+		Function(double a, double b){
+			this.a = a;
+			this.b = b;			
+		}
+		
+		Function(double a){
+			this.a = a;
+		}
+		
+		abstract double function();
+	}
+	
+	class FunctionPlus extends Function{
+
+		FunctionPlus(double a, double b) {
+			super(a, b);
+		}
+
+		@Override
+		double function() {
+			return a+b;
+		}		
+	}
+	
+	class FunctionMinus extends Function{
+
+		FunctionMinus(double a, double b) {
+			super(a, b);
+		}
+
+		@Override
+		double function() {
+			return a-b;
+		}		
+	}
+	
+	class FunctionMultiply extends Function{
+
+		FunctionMultiply(double a, double b) {
+			super(a, b);
+		}
+
+		@Override
+		double function() {
+			return a*b;
+		}		
+	}
+	
+	class FunctionDivide extends Function{
+
+		FunctionDivide(double a, double b) {
+			super(a, b);
+		}
+
+		@Override
+		double function() {
+			return a/b;
+		}		
+	}
+	
+	class FunctionSqrt extends Function{
+
+		FunctionSqrt(double a) {
+			super(a);
+		}
+
+		@Override
+		double function() {
+			return Math.sqrt(a);
+		}		
+	}
+	
+	private void operation(Function function) {
 		try {
-			Double result = a + b;		
+			Double result = function.function();		
 			Thread.sleep(delay);		
 			if(Double.isInfinite(result))
 				throw new UnsupportedOperationException("zero divide");
@@ -28,81 +104,25 @@ public class Application {
 		catch(UnsupportedOperationException e) {
 			System.err.println(e.getLocalizedMessage());		
 		}		
+	}
+	
+	public void plus(double a, double b) {
+		operation(new FunctionPlus(a, b));	
 	}
 	
 	public void minus(double a, double b) {
-		try {
-			Double result = a - b;		
-			Thread.sleep(delay);		
-			if(Double.isInfinite(result))
-				throw new UnsupportedOperationException("zero divide");
-			else if(Double.isNaN(result))
-				throw new UnsupportedOperationException("complex result");
-			else
-				System.out.println(result);
-		}
-		catch(InterruptedException e) {
-			System.err.println(e.getLocalizedMessage());			
-		}
-		catch(UnsupportedOperationException e) {
-			System.err.println(e.getLocalizedMessage());		
-		}						
+		operation(new FunctionMinus(a, b));
 	}
 	
 	public void multiply(double a, double b) {
-		try {
-			Double result = a * b;		
-			Thread.sleep(delay);		
-			if(Double.isInfinite(result))
-				throw new UnsupportedOperationException("zero divide");
-			else if(Double.isNaN(result))
-				throw new UnsupportedOperationException("complex result");
-			else
-				System.out.println(result);
-		}
-		catch(InterruptedException e) {
-			System.err.println(e.getLocalizedMessage());			
-		}
-		catch(UnsupportedOperationException e) {
-			System.err.println(e.getLocalizedMessage());		
-		}			
+		operation(new FunctionMultiply(a, b));
 	}
 	
 	public void divide(double a, double b) {
-		try {
-			Double result = a / b;		
-			Thread.sleep(delay);		
-			if(Double.isInfinite(result))
-				throw new UnsupportedOperationException("zero divide");
-			else if(Double.isNaN(result))
-				throw new UnsupportedOperationException("complex result");
-			else
-				System.out.println(result);
-		}
-		catch(InterruptedException e) {
-			System.err.println(e.getLocalizedMessage());			
-		}
-		catch(UnsupportedOperationException e) {
-			System.err.println(e.getLocalizedMessage());		
-		}		
+		operation(new FunctionDivide(a, b));
 	}
 	
 	public void sqrt(double a) {
-		try {
-			Double result = Math.sqrt(a);		
-			Thread.sleep(delay);		
-			if(Double.isInfinite(result))
-				throw new UnsupportedOperationException("zero divide");
-			else if(Double.isNaN(result))
-				throw new UnsupportedOperationException("complex result");
-			else
-				System.out.println(result);
-		}
-		catch(InterruptedException e) {
-			System.err.println(e.getLocalizedMessage());			
-		}
-		catch(UnsupportedOperationException e) {
-			System.err.println(e.getLocalizedMessage());		
-		}			
+		operation(new FunctionSqrt(a));
 	}
 }
