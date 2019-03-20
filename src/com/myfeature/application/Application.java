9a4e9a714b40f50d60335082499870fc38c837f0
millet.ua.su@gmail.com <millet.ua.su@gmail.com>
2019-03-20 23:50:12 +0200

    Lambda expression as delegate
    Demo of lambda expressions

--- src/com/myfeature/application/Application.java
+++ src/com/myfeature/application/Application.java
@@ -6,7 +6,7 @@ import com.myfeature.functions.Function;
  *	problem #1: unstable work, throwing exceptions; - RESOLVED
  *	problem #2: passing problems to 3rd-party developers; -RESOLVED
  *	problem #3: delays during work;
- *	problem #4: duplicated code; 
+ *	problem #4: duplicated code; - RESOLVED
  *	problem #5: encapsulate display logic(only console display);
  */
 public class Application {
@@ -33,47 +33,22 @@ public class Application {
 	}
 	
 	public void plus(double a, double b) {
-		operation(new Function<Double>() {			
-			@Override
-			public Double function() {				
-				return a+b;
-			}
-		});	
+		operation(() -> a + b);	
 	}
 	
 	public void minus(double a, double b) {
-		operation(new Function<Double>() {
-			@Override
-			public Double function() {				
-				return a-b;
-			}
-		});
+		operation(() -> a - b);
 	}
 	
 	public void multiply(double a, double b) {
-		operation(new Function<Double>() {
-			@Override
-			public Double function() {				
-				return a*b;
-			}
-		});
+		operation(() -> a * b);
 	}
 	
 	public void divide(double a, double b) {
-		operation(new Function<Double>() {			
-			@Override
-			public Double function() {				
-				return a/b;
-			}
-		});
+		operation(() -> a / b);
 	}
 	
 	public void sqrt(double a) {
-		operation(new Function<Double>() {			
-			@Override
-			public Double function() {				
-				return Math.sqrt(a);
-			}
-		});
+		operation(() -> Math.sqrt(a));
 	}
 }
